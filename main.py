b9a991e4a1dcb6b285b85aafcb2111169c40bdbf
danil <danil.vladimirow2016@yandex.ru>
2022-03-21 23:40:56 +0300

    решил последнюю классную(на создание формы) из того же урока

--- main.py
+++ main.py
@@ -1,19 +1,119 @@
-from flask import Flask
-from flask import *
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Привет, Яндекс!"
+@app.route('/astranaut_selection', methods=['POST', 'GET'])
+def form_flask():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор остранавтов</title>
+                          </head>
+                          <body>
+                            <h2Форма для регистрации в </h2">
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input type="surname" class="form-control" id="surname" aria-describedby="emailHelp" placeholder="Введите фамилию" name="surname">
+                                    <input type="name" class="form-control" id="name" placeholder="Введите имя" name="name">
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
 
-@app.route('/image_mars')
-def ddd():
-    return f'''<h1>Жди нас, Марс!<h1><img src="{url_for('static', filename='photo.png')}" 
-           alt="здесь должна была быть картинка, но не нашлась"><h3>Вот она какая, красная планета<h3>'''
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у вас образование?</label>
+                                        <select class="form-control" id="school" name="school">
+                                          <option>Высшее</option>
+                                          <option>Среднее</option>
+                                          <option>Начальное</option>
+                                        </select>
+                                     </div>
 
+                                     <div class="form-group">
+                                        <label for="form-check">ваши профессии?</label>
+                                    </div>
+
+                                     <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules1" name="accept">
+                                        <label class="form-check-label" for="acceptRules1">Инженер-исследователь</label>
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules2" name="accept">
+                                        <label class="form-check-label" for="acceptRules2">Пилот</label>
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules3" name="accept">
+                                        <label class="form-check-label" for="acceptRules3">Экзобиолог</label>
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules4" name="accept">
+                                        <label class="form-check-label" for="acceptRules4">Врач</label>
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules5" name="accept">
+                                        <label class="form-check-label" for="acceptRules5">Геолог</label>
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules6" name="accept">
+                                        <label class="form-check-label" for="acceptRules5">психолог-суетолог</label>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="dk" value="another">
+                                          <label class="form-check-label" for="another">
+                                            another
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему хотите принимать участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules6" name="accept">
+                                        <label class="form-check-label" for="acceptRules6">Готовы остаться на Марсе, после завершения экспедиции?</label>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.form['name'])
+        print(request.form['surname'])
+        print(request.form['email'])
+        print(request.form['school'])
+        print(request.form['file'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
