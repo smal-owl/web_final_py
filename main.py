fcc7762894637f6132cdd74a7039aece4e387472
danil <danil.vladimirow2016@yandex.ru>
2022-03-22 00:07:59 +0300

    решил первый доп, на загрузку  и последующий файла

--- main.py
+++ main.py
@@ -3,117 +3,68 @@ from flask import Flask, url_for, request
 app = Flask(__name__)
 
 
-@app.route('/astranaut_selection', methods=['POST', 'GET'])
-def form_flask():
+#  http://127.0.0.1:8080/results/Егор/3/3.4
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def sample_file_upload():
     if request.method == 'GET':
         return f'''<!doctype html>
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
                             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Отбор остранавтов</title>
+                            <title>Пример загрузки файла</title>
                           </head>
                           <body>
-                            <h2Форма для регистрации в </h2">
-                            <div>
-                                <form class="login_form" method="post">
-                                    <input type="surname" class="form-control" id="surname" aria-describedby="emailHelp" placeholder="Введите фамилию" name="surname">
-                                    <input type="name" class="form-control" id="name" placeholder="Введите имя" name="name">
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                            <h1>Загрузим файл</h1>
+                            <form method="post" enctype="multipart/form-data">
+                               <div class="form-group">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
 
-                                    <div class="form-group">
-                                        <label for="classSelect">Какое у вас образование?</label>
-                                        <select class="form-control" id="school" name="school">
-                                          <option>Высшее</option>
-                                          <option>Среднее</option>
-                                          <option>Начальное</option>
-                                        </select>
-                                     </div>
-
-                                     <div class="form-group">
-                                        <label for="form-check">ваши профессии?</label>
-                                    </div>
-
-                                     <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules1" name="accept">
-                                        <label class="form-check-label" for="acceptRules1">Инженер-исследователь</label>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules2" name="accept">
-                                        <label class="form-check-label" for="acceptRules2">Пилот</label>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules3" name="accept">
-                                        <label class="form-check-label" for="acceptRules3">Экзобиолог</label>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules4" name="accept">
-                                        <label class="form-check-label" for="acceptRules4">Врач</label>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules5" name="accept">
-                                        <label class="form-check-label" for="acceptRules5">Геолог</label>
-                                    </div>
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules6" name="accept">
-                                        <label class="form-check-label" for="acceptRules5">психолог-суетолог</label>
-                                    </div>
-
-
-                                    <div class="form-group">
-                                        <label for="form-check">Укажите пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="dk" value="another">
-                                          <label class="form-check-label" for="another">
-                                            another
-                                          </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="about">Почему хотите принимать участие в миссии?</label>
-                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules6" name="accept">
-                                        <label class="form-check-label" for="acceptRules6">Готовы остаться на Марсе, после завершения экспедиции?</label>
-                                    </div>
-
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                </form>
-                            </div>
+                            <img src="{url_for('static', filename='img/map.png')}"alt="здесь мгола быть ваша реклама">
+                            </form>
                           </body>
                         </html>'''
     elif request.method == 'POST':
-        print(request.form['name'])
-        print(request.form['surname'])
-        print(request.form['email'])
-        print(request.form['school'])
-        print(request.form['file'])
-        print(request.form['accept'])
-        print(request.form['sex'])
-        return "Форма отправлена"
+        f = request.files['file']
+        fff = f.read()
+        map_file = "static/map.png"
+        with open(map_file, "wb") as file:
+            file.write(fff)
+            file.close()
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <form method="post" enctype="multipart/form-data">
+                               <div class="form-group">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                            <img src="{url_for('static', filename='map.png')}">
+                            </form>
+                          </body>
+                        </html>'''
 
 
 if __name__ == '__main__':
